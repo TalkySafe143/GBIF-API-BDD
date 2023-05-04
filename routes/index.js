7bcddd283505c3d9a69023193f8c0674d17eb635
Sebastian Galindo <sebastian.galindo143@gmail.com>
2023-05-04 00:59:59 -0500

    Creating connection to Oracle Database

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const oracleClient = require('../lib/oracle/oracle');
 
-router.get('/', function(req, res, next) {
-  res.json({
-    hola: 'Hoila'
-  })
+router.get('/', async function(req, res, next) {
+
+  const oracle = new oracleClient();
+
+  res.json(await oracle.getAll('STUDENT'));
 });
 
 module.exports = router;
