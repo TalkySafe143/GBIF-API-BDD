bd78378fea3f014d03ada7220f2f5cbef6817a58
Sebastian Galindo <sebastian.galindo143@gmail.com>
2023-05-04 01:00:19 -0500

    Creating connection to Oracle Database 2.0

---
+++ config/config.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+
+module.exports = {
+    userOracle : process.env.USER_DB_ORACLE,
+    passwordOracle: process.env.PASSWORD_DB_ORACLE,
+    urlConnectionOracle: process.env.CONNECTION_URL_DB_ORACLE,
+    configDir: 'C:\\\\Users\\\\PC\\\\Desktop\\\\Carpeta Sebastian\\\\instantclient-basic-windows.x64-21.9.0.0.0dbru\\\\instantclient_21_9'
+}
